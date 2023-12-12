43f6fcb94df8f249ca97fdf3a068c8925ab0f9c4
Samuele Macchi <macchisam@gmail.com>
2023-12-12 20:06:54 +0100

    Fix

--- set.c
+++ set.c
@@ -31,11 +31,6 @@ size_t djb33x_hash(const char *key, const size_t keylen)
     return hash;
 }
 
-void generate_unique_key(struct aiv_set *set,char *key_buffer, size_t buffer_size)
-{
-    snprintf(key_buffer,buffer_size,"KEY:%d",set->counter++);
-}
-
 void aiv_set_insert(struct aiv_set *set, const char *key)
 {
     const size_t key_len = strlen(key);
@@ -46,46 +41,24 @@ void aiv_set_insert(struct aiv_set *set, const char *key)
 
     printf("hash of %s = %llu (index: %llu)\n", key, hash, index);
 
+
     for (size_t i = 0; i < HASHMAP_SIZE_LIST; i++)
     {
+       
         if (set->hashmap[index][i].key_len == 0)
         {
             set->hashmap[index][i].key = key;
             set->hashmap[index][i].key_len = key_len;
             printf("added %s at index %llu slot %llu\n", key, index, i);
             return;
+        }else if (set->hashmap[index][i].key_len == key_len && memcmp(set->hashmap[index][i].key, key, key_len) == 0)
+        {
+            printf("Key %s already exists at index %llu slot %llu\n", key, index, i);
+            return;
         }
     }
 
     printf("COLLISION! for %s (index %llu)\n", key, index);
-}
-
-void aiv_set_insert_unique(struct aiv_set *set, char *key)
-{
-    char unique_key[BUFFER_SIZE];
-    
-    while(1)
-    {
-        generate_unique_key(set,unique_key,BUFFER_SIZE);
-
-        const size_t key_len = strlen(key);
-
-        const size_t hash = djb33x_hash(key, key_len);
-
-        const size_t index = hash % HASHMAP_SIZE;
-
-        for (size_t i = 0; i < HASHMAP_SIZE_LIST; i++)
-        {
-            if(set->hashmap[index][i].key_len == 0)
-            {
-                set->hashmap[index][i].key = unique_key;
-                set->hashmap[index][i].key_len = key_len;
-                printf("added %s at index %llu slot %llu\n", key, index, i);
-                return;
-            }
-        }
-
-    }
 }
 
 void aiv_set_remove(struct aiv_set *set, const char *key)
@@ -147,11 +120,10 @@ int main(int argc, char **argv)
     aiv_set_insert(&myset, "Foobar");
     aiv_set_insert(&myset, "XYZ");
     aiv_set_insert(&myset, "AAAAAA");
-    aiv_set_insert(&myset, "AAAAAa");
+    aiv_set_insert(&myset, "AAAAAA");
     aiv_set_find(&myset, "XYZ");
     aiv_set_remove(&myset,"Test");
     aiv_set_find(&myset,"Test");
-    aiv_set_insert_unique(&myset,"TEST12345");
 
     return 0;
 }
